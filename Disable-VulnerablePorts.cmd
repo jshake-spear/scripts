7cda727626c1940ed58494d28766e7531dd9112b
jshake-spear <57912180+jshake-spear@users.noreply.github.com>
2023-04-05 11:46:16 -0600

    Fix: Powershell command
    The PowerShell command was not properly running from cmd.exe. This has been fixed/verified.

--- Disable-VulnerablePorts.cmd
+++ Disable-VulnerablePorts.cmd
@@ -2,4 +2,4 @@
 :: File must be run with administrator privileges
 netsh advfirewall firewall set rule group="Remote Administration" new enable=no
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=no
-powershell.exe -command {Get-NetFirewallPortFilter | Where-Object { $_.LocalPort -eq 135 -or $_.LocalPort -eq 137 -or $_.LocalPort -eq 138 -or $_.LocalPort -eq 139 } | get-netfirewallrule | Where-Object { $_.Group } | Disable-NetFirewallRule }
+powershell.exe -command " & {Get-NetFirewallPortFilter | Where-Object { $_.LocalPort -eq 135 -or $_.LocalPort -eq 137 -or $_.LocalPort -eq 138 -or $_.LocalPort -eq 139 } | get-netfirewallrule | Where-Object { $_.Group } | Disable-NetFirewallRule }"
